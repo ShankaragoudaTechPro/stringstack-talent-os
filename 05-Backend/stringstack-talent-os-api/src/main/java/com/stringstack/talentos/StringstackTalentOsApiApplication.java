082cb362874f6dec5e94613925b38dd58cb6bd7f
Shankaragouda <shankaragouda@gmail.com>
2026-07-01 12:17:06 +0530

    Setup Spring Boot backend project with MySQL configuration

---
+++ 05-Backend/stringstack-talent-os-api/src/main/java/com/stringstack/talentos/StringstackTalentOsApiApplication.java
@@ -0,0 +1,13 @@
+package com.stringstack.talentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringstackTalentOsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringstackTalentOsApiApplication.class, args);
+    }
+
+}
---
+++ 05-Backend/stringstack-talent-os-api/src/test/java/com/stringstack/talentos/StringstackTalentOsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stringstack.talentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringstackTalentOsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
